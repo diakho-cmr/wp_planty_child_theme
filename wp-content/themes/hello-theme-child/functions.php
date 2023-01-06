7e426e3306e374252045ea9d8f76aa2f3a80208d
Diakho Camara <diakho.camara@gmail.com>
2023-01-06 11:51:52 +0100

    create header and footer

--- wp-content/themes/hello-theme-child/functions.php
+++ wp-content/themes/hello-theme-child/functions.php
@@ -10,14 +10,25 @@
  *
  * @return void
  */
+
 function hello_elementor_child_enqueue_scripts() {
 	wp_enqueue_style(
 		'hello-elementor-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		[
-			'hello-elementor-theme-style',
-		],
-		'1.0.0'
-	);
+		get_stylesheet_directory_uri() . '/style.css');
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+/*
+function custom_logo_setup() {
+	$args = [
+		'height'               => 19,
+		'width'                => 192,
+		'flex-height'          => true,
+		'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+		'unlink-homepage-logo' => true, 
+	];
+	add_theme_support( 'custom-logo', $args );
+}
+add_action( 'after_setup_theme', 'custom_logo_setup' );
+*/
